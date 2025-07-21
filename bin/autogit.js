1ee7bd58afdeb50f77f95e147381f073d2f4c028
shubham karne <120401166+shubhamkarne11@users.noreply.github.com>
2025-07-21 07:26:30 +0530

    auto-commit

---
+++ bin/autogit.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+import chalk from 'chalk';
+import ora from 'ora';
+import { askSetup, loadConfig } from '../lib/config.js';
+import { initGitRepo, commitAndPushChanges } from '../lib/gitUtils.js';
+import startWatcher from '../lib/watcher.js';
+//
+console.clear();
+console.log(chalk.cyan.bold('⚡ AutoGit CLI – GitHub Auto Commit & Push'));
+
+let config = loadConfig();
+
+if (!config) {
+  config = await askSetup();
+  await initGitRepo(config.repo);
+}
+
+if (config.mode === 'Automatically') {
+  console.log(chalk.green('🔄 Watching for changes...'));
+  startWatcher(() => commitAndPushChanges('auto-commit'));
+} else {
+  const spinner = ora('Committing manually...').start();
+  await commitAndPushChanges('manual-commit');
+  spinner.succeed('✅ Manual commit pushed!');
+}
